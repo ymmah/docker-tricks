7b4de90e80c2cb4df2dbcef8cb9985604a69508a
Jeremiah Norris <jeremiah.norris@acxiom.com>
2018-03-05 12:22:57 -0600

    change artifactory-oss port

--- powershell/run-artifactory-oss.ps1
+++ powershell/run-artifactory-oss.ps1
@@ -5,6 +5,6 @@ docker rm artifactory-oss
 # runs the latest docker image, downloading it if needed
 docker run `
   -d `
-  -p 8081:8081 `
+  -p 8084:8081 `
   --name artifactory-oss `
   docker.bintray.io/jfrog/artifactory-oss:latest
