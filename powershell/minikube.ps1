931c401853c3bce034d4b76e1a618e527c44a87c
Jeremiah Norris <jeremiah.norris@acxiom.com>
2018-03-07 08:37:25 -0600

    convert minikube-start to generic minikube goodness

--- powershell/minikube.ps1
+++ powershell/minikube.ps1
@@ -1,3 +1,6 @@
 # "minikube" is just what I decided to name my virtual switch
 # see instructions here: https://docs.docker.com/machine/drivers/hyper-v/#2-set-up-a-new-external-network-switch-optional
-minikube start --vm-driver hyperv --hyperv-virtual-switch "minikube"
+minikube destroy
+minikube config set vm-driver hyperv
+minikube config set hyperv-virtual-switch minikube
+minikube start
