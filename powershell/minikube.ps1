50b32d274419917ca2645d1d7b0a4121e16ede7c
Jeremiah Norris <jeremiah.norris@acxiom.com>
2018-03-07 08:37:54 -0600

    destroy is not a command

--- powershell/minikube.ps1
+++ powershell/minikube.ps1
@@ -1,6 +1,6 @@
 # "minikube" is just what I decided to name my virtual switch
 # see instructions here: https://docs.docker.com/machine/drivers/hyper-v/#2-set-up-a-new-external-network-switch-optional
-minikube destroy
+minikube delete
 minikube config set vm-driver hyperv
 minikube config set hyperv-virtual-switch minikube
 minikube start
